de9f72946fb25e3587ac5dc0be152ed3c7001c14
fdhliakbar <fadhliakbar125@gmail.com>
2023-06-28 02:06:42 +0700

    add the features login

--- index.php
+++ index.php
@@ -5,10 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <script src="https://unpkg.com/feather-icons"></script>
+    <link rel="stylesheet" href="public/css/home.css">
 </head>
 
 <body>
+    <!--  -->
 
+    <footer>
+        @copyright fdhliakbar 2023
+    </footer>
+
+    <!-- Feather icons -->
+    <script>
+    feather.replace();
+    </script>
+
+    <!-- Javascript file -->
+    <script src="public/js/home/js"></script>
 </body>
 
 </html>
