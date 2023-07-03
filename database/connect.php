7db3dbf2b2ea9ce4c38212a1afc3c8dbc91b7821
fdhliakbar <fadhliakbar125@gmail.com>
2023-07-03 13:33:15 +0700

    berhasil connect ke MySql

--- database/connect.php
+++ database/connect.php
@@ -4,25 +4,19 @@ $username = "root";
 $password = "";
 $dbname = "delivery_ordering";
 
-// Membuat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Memeriksa koneksi
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
+} else {
+    echo "Koneksi berhasil";
 }
 
 // Query dan penggunaan data dari tabel
 $sql = "SELECT * FROM nama_tabel";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "Nama: " . $row["nama"]. " - Email: " . $row["email"]. "<br>";
-    }
-} else {
-    echo "0 hasil";
-}
 
 // Menutup koneksi
 $conn->close();
