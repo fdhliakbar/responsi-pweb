e3bf3cec30b8e6ceda0626ed79e432cefc3c3eb7
fdhliakbar <fadhliakbar125@gmail.com>
2023-07-03 14:07:27 +0700

    membuat file header navbar

--- index.php
+++ index.php
@@ -10,62 +10,16 @@
     <link rel="shortcut icon" href="components/assets/image/logo/favicon.ico" type="image/x-icon" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <script src="https://unpkg.com/feather-icons"></script>
     <link rel="stylesheet" href="./components/css/home.css" />
+    <script src="https://unpkg.com/feather-icons"></script>
+    <script defer src="./components/js/scroll.js"></script>
 </head>
 
 <body>
     <!-- Navbar start -->
-    <nav class="navbar">
-        <a href="#" class="navbar-logo">Gomu <span>No Mi</span></a>
-
-        <div class="navbar-nav">
-            <a href="#">Home</a>
-            <a href="#about">About</a>
-            <a href="#menu">Menu</a>
-            <a href="#contact">Contact</a>
-        </div>
-
-        <div class="navbar-extra">
-            <a href="#" id="search"><i data-feather="search"></i></a>
-            <a href="#" id="shopping-cart"><i data-feather="shopping-cart"></i></a>
-            <a href="#" id="hamburger-menu"><i data-feather="menu"></i></a>
-        </div>
-
-        <!-- Search -->
-        <div class="search-form">
-            <input type="search" id="search-box" placeholder="search here..." />
-            <label for="search-box"><i data-feather="search"></i></label>
-        </div>
-
-        <!-- Shopping cart -->
-        <div class="shopping-cart">
-            <div class="cart-item">
-                <img src="components/assets/image/food2.jpg" alt="Product 1" />
-                <div class="item-detail">
-                    <h3>Product 1</h3>
-                    <div class="item-price">IDR 30K</div>
-                </div>
-                <i data-feather="trash-2" class="remove-item"></i>
-            </div>
-            <div class="cart-item">
-                <img src="components/assets/image/food2.jpg" alt="Product 1" />
-                <div class="item-detail">
-                    <h3>Product 1</h3>
-                    <div class="item-price">IDR 30K</div>
-                </div>
-                <i data-feather="trash-2" class="remove-item"></i>
-            </div>
-            <div class="cart-item">
-                <img src="components/assets/image/food2.jpg" alt="Product 1" />
-                <div class="item-detail">
-                    <h3>Product 1</h3>
-                    <div class="item-price">IDR 30K</div>
-                </div>
-                <i data-feather="trash-2" class="remove-item"></i>
-            </div>
-        </div>
-    </nav>
+    <?php
+    include './database/navbar.php';
+    ?>
     <!-- Navbar end -->
 
     <!-- Hero Section start -->
