58b23a8b3e7a936f78a9f19a8ff7fe0b34d39b77
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-07-07 01:19:06 +0700

    push

--- menu.php
+++ menu.php
@@ -7,29 +7,34 @@
             <p class="menu-card-price">IDR 200K</p>
         </div>
         <div class="menu-card">
-            <img src="components/assets/image/food1.jpg" alt="Sushi" class="menu-card-img" />
-            <h3 class="menu-card-title">- Sushi -</h3>
-            <p class="menu-card-price">IDR 200K</p>
+            <img src="components/assets/image/menu2.jpg" alt="Ramen" class="menu-card-img" />
+            <h3 class="menu-card-title">- Ramen -</h3>
+            <p class="menu-card-price">IDR 125K</p>
+        </div>
+        <div class="menu-card">
+            <img src="components/assets/image/menu3.jpg" alt="Tofu" class="menu-card-img" />
+            <h3 class="menu-card-title">- Tofu -</h3>
+            <p class="menu-card-price">IDR 25K</p>
+        </div>
+        <div class="menu-card">
+            <img src="components/assets/image/menu4.webp" alt="Omelet" class="menu-card-img" />
+            <h3 class="menu-card-title">- Omelet -</h3>
+            <p class="menu-card-price">IDR 20K</p>
         </div>
         <div class="menu-card">
             <img src="components/assets/image/food1.jpg" alt="Sushi" class="menu-card-img" />
             <h3 class="menu-card-title">- Sushi -</h3>
-            <p class="menu-card-price">IDR 200K</p>
+            <p class="menu-card-price">IDR 80K</p>
         </div>
         <div class="menu-card">
-            <img src="components/assets/image/food1.jpg" alt="Sushi" class="menu-card-img" />
-            <h3 class="menu-card-title">- Sushi -</h3>
-            <p class="menu-card-price">IDR 200K</p>
+            <img src="components/assets/image/menu5.jpg" alt="Takoyaki" class="menu-card-img" />
+            <h3 class="menu-card-title">- Takoyaki -</h3>
+            <p class="menu-card-price">IDR 30K</p>
         </div>
         <div class="menu-card">
-            <img src="components/assets/image/food1.jpg" alt="Sushi" class="menu-card-img" />
-            <h3 class="menu-card-title">- Sushi -</h3>
-            <p class="menu-card-price">IDR 200K</p>
-        </div>
-        <div class="menu-card">
-            <img src="components/assets/image/food1.jpg" alt="Sushi" class="menu-card-img" />
-            <h3 class="menu-card-title">- Sushi -</h3>
-            <p class="menu-card-price">IDR 200K</p>
+            <img src="components/assets/image/menu6.jpg" alt="Dango" class="menu-card-img" />
+            <h3 class="menu-card-title">- Dango -</h3>
+            <p class="menu-card-price">IDR 12K</p>
         </div>
     </div>
 </section>
@@ -45,10 +50,10 @@
                 <a href="#" class="item-detail-button"><i data-feather="eye"></i></a>
             </div>
             <div class="product-image">
-                <img src="components/assets/image/food2.jpg" alt="Product 1" />
+                <img src="components/assets/image/menu6.jpg" alt="Product 1" />
             </div>
             <div class="product-content">
-                <h3>Matcha tea</h3>
+                <h3>Dango</h3>
                 <div class="product-stars">
                     <i data-feather="star" class="star-full"></i>
                     <i data-feather="star" class="star-full"></i>
