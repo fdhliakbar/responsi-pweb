14d6cebe0a09e53aebe038eee5275b4d9a40069a
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-07-16 18:55:38 +0700

    terakhir deh

--- db.php
+++ db.php
@@ -4,13 +4,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $number = $_POST['number'];
   $nama = $_POST['nama'];
 
-  $data = "Nama: $nama" . PHP_EOL . "jumlah: $number" . PHP_EOL . "Nama produk yang dibeli: $product" . PHP_EOL . PHP_EOL;
+  $data = "Nama: $nama" . PHP_EOL . "Jumlah: $number" . PHP_EOL . "Nama produk yang dibeli: $product" . PHP_EOL . PHP_EOL;
 
   $file = fopen('order.txt', 'a');
   fwrite($file, $data);
   fclose($file);
 
-  echo "<h2>Terima kasih!</h2>";
-  echo "<p>Pembelian Anda telah berhasil. Data pembelian telah disimpan.</p>";
+  header("Location: index.html");
 }
 ?>
